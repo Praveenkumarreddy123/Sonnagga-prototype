e8c828a7925c03d6fb0d7b58c33393d517487dc3
Praveenkumarreddy123 <praveenkumarreddy9894@gmail.com>
2019-05-02 01:41:21 +0530

    Home Page Update

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:4,
+        lazyLoad:true,
+        loop:true,
+        autoplay:true,
+        dots: false,
+        nav: true
+    });
+});
